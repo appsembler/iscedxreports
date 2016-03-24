285522e51bdb12647bfabef7da5009594c383354
Bryan Wilson <bryanlandia@gmail.com>
2016-03-23 22:48:39 -0700

    Add max_length attrs to extension fields

--- iscedxreports/models.py
+++ iscedxreports/models.py
@@ -6,6 +6,6 @@ class InterSystemsUserProfile(models.Model):
     """User profile fields specific to InterSystems
     """
     user = models.ForeignKey(User, db_index=True, related_name="InterSystemspreferences")
-    organization = models.CharField(verbose_name='Organization', blank=False)
-    job_title = models.CharField(verbose_name='Job Title', blank=True)
+    organization = models.CharField(verbose_name='Organization', blank=False, max_length=100)
+    job_title = models.CharField(verbose_name='Job Title', blank=True, max_length=100)
 	
