a892a11a51be70af3a873f6e26acbfef4e285ed2
Bryan Wilson <bryanlandia@gmail.com>
2016-08-05 15:39:55 -0700

    Force the registration of certificates models for 0004 migration

--- iscedxreports/migrations/0004_auto_20160412_2240.py
+++ iscedxreports/migrations/0004_auto_20160412_2240.py
@@ -11,12 +11,16 @@ def enable_auto_certificate_generation(apps, schema_editor):
         enable student-generated certificates on all courses
         by setting the enabled bit
     """
+    # not sure why I'm not getting the certificates app, so we'll force it
+    from certificates.models import CertificateGenerationConfiguration, CertificateGenerationCourseSetting
     apps.load_app('certificates')
-    CertificateGenerationConfiguration = apps.get_model("lms.djangoapps.certificates", "CertificateGenerationConfiguration")
+    apps.register_models('certificates', CertificateGenerationConfiguration)
+    apps.register_models('certificates', CertificateGenerationCourseSetting)
+    CertificateGenerationConfiguration = apps.get_model("certificates", "CertificateGenerationConfiguration")
     ccg = CertificateGenerationConfiguration(enabled=1)  # configuration model.  just add a new one
     ccg.save()
     
-    CertificateGenerationCourseSetting = apps.get_model("lms.djangoapps.certificates", "CertificateGenerationCourseSetting")
+    CertificateGenerationCourseSetting = apps.get_model("certificates", "CertificateGenerationCourseSetting")
     courses = modulestore().get_courses()
     for course in courses:
         key = course.location.course_key
@@ -30,7 +34,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('iscedxreports', '0003_auto_20160327_2004'),
-        ('certificates', '__first__'),
+        ('certificates', '__latest__'),
     ]
 
     operations = [
