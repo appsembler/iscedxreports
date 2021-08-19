98b5bb0c3b3645576b5c00304575ee311c8ff61f
Bryan Wilson <bryanlandia@gmail.com>
2021-08-19 21:52:07 +0000

    Fix urls for Django2

--- iscedxreports/urls.py
+++ iscedxreports/urls.py
@@ -1,14 +1,12 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from iscedxreports import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(
-        r'^courses/{}/feedback$'.format(
-            settings.COURSE_ID_PATTERN,
-        ),
-        'iscedxreports.views.course_feedback_tab_view',
-        name='course_feedback_tab_view',
+        r'^courses/{}/feedback$'.format(settings.COURSE_ID_PATTERN),
+        views.course_feedback_tab_view, name='course_feedback_tab_view'
     ),
-)
+]
